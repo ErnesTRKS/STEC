a1c992e5ab504556119282909590b1df2c96d8bc
ErnesTRKS <ernesto.ts06@gmail.com>
2021-03-19 01:26:58 -0600

    Update Padding.py

--- Actividades/Padding.py
+++ Actividades/Padding.py
@@ -8,6 +8,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+def processImage(image):
+    image = cv2.imread(image)
+    image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
+    return image
 
 def convolution(image, kernel, average=False, verbose=False):
     if len(image.shape) == 3:
@@ -56,8 +60,8 @@ def convolution(image, kernel, average=False, verbose=False):
 
     return output
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
 
-        image = processImage('Image.jpg')
-        kernel = np.ones([3,3])
-        output = convolution(image, kernel, average=False, verbose=False)
+    image = processImage('Image.jpg')
+    kernel = np.ones([3,3])
+    output = convolution(image, kernel, average=True, verbose=True)
